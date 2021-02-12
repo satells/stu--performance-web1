a3fb76f72ddb7441503dacd33697f1377acd0100
satells <msergiost@hotmail.com>
2021-02-12 15:38:43 -0300

    alterado busca

--- site/assets/js/busca.js
+++ site/assets/js/busca.js
@@ -1,38 +1,36 @@
-window.addEventListener('load', function() {
-	var botaoBusca = document.querySelector('.header-busca');
-	var body = document.body;
-	var navegacaoForm = document.querySelector('.header-navegacao-form');
+window.addEventListener("load", function () {
+    var botaoBusca = document.querySelector(".header-busca");
+    var body = document.body;
+    var navegacaoForm = document.querySelector(".header-navegacao-form");
 
-	if (botaoBusca && "classList" in document.documentElement) {	
+    if (botaoBusca && "classList" in document.documentElement) {
+        function toggleSearch(event) {
+            body.classList.toggle("header-barraBusca-visivel");
+            navegacaoForm.classList.toggle("navegacao-form-active");
+            event.preventDefault();
+        }
 
-		function toggleSearch(event) {
-			body.classList.toggle('header-barraBusca-visivel');
-			navegacaoForm.classList.toggle('navegacao-form-active');
-			event.preventDefault();
-		}
+        function closeSearch() {
+            body.classList.remove("header-barraBusca-visivel");
+            navegacaoForm.classList.remove("navegacao-form-active");
+        }
 
-		function closeSearch() {
-			body.classList.remove('header-barraBusca-visivel');	
-			navegacaoForm.classList.remove('navegacao-form-active');
-		}
+        function cancelPropagation(event) {
+            event.stopPropagation();
+            console.log("fdsadf");
+        }
 
-		function cancelPropagation(event) {
-			event.stopPropagation();
-		}
+        function setupCloseSearch() {
+            setTimeout(function () {
+                document.documentElement.addEventListener("click", closeSearch);
+                document.querySelector(".header-navegacao").addEventListener("click", cancelPropagation);
+                document.querySelector(".header-barraBusca").addEventListener("click", cancelPropagation);
+            }, 0);
 
-		function setupCloseSearch() {
-			setTimeout(function(){
-				document.documentElement.addEventListener('click', closeSearch);
-				document.querySelector('.header-navegacao').addEventListener('click', cancelPropagation);
-				document.querySelector('.header-barraBusca').addEventListener('click', cancelPropagation);
-			}, 0);
+            botaoBusca.removeEventListener("click", setupCloseSearch);
+        }
 
-			botaoBusca.removeEventListener('click', setupCloseSearch);
-		}
-
-		botaoBusca.addEventListener('click', toggleSearch);
-		botaoBusca.addEventListener('click', setupCloseSearch);
-	}
-
-
+        botaoBusca.addEventListener("click", toggleSearch);
+        botaoBusca.addEventListener("click", setupCloseSearch);
+    }
 });
